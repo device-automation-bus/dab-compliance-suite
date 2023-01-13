fe19b60e142ef15ad9c40f254aad17ed5739d25d
Denis Yuji Shimizu <denis.shimizu@collabora.com>
2023-01-13 14:51:18 -0300

    add lock mechanism

--- main.py
+++ main.py
@@ -1,42 +1,50 @@
 from time import sleep
+from threading import Lock
 import json
 from paho.mqtt.properties import Properties
 from paho.mqtt.packettypes import PacketTypes 
 import paho.mqtt.client as mqtt
 
 class Dab_Client:
-    def on_message(self, client, userdata, message):
-        print("Received: ")    
-        json_object = json.loads(message.payload)
-        json_formatted_str = json.dumps(json_object, indent=2)
-        print(json_formatted_str)
-        print("on topic: ",message.topic)
-        self.disconnect()
+    def __init__(self):
+        self.lock = Lock()
+        self.lock.acquire()
+        self.client = mqtt.Client("mqtt5_client",protocol=mqtt.MQTTv5) 
+
+    def on_message(self, client, userdata, message):   
+        self.response = json.loads(message.payload)
+        self.lock.release()
 
     def disconnect(self):
         self.client.disconnect()
 
     def connect(self,broker_address,broker_port):
-        print("Creating new Client instance")
-        self.client = mqtt.Client("mqtt5_client",protocol=mqtt.MQTTv5) 
-        print("Connecting to MQTT broker")
+        print("Connecting to MQTT broker at",broker_address,":",broker_port)
         self.client.connect(broker_address, port=broker_port)
         self.client.loop_start()
     
-    def send_cmd(self,topic,msg):
+    def request(self,topic,msg="{}"):
+        print("Requesting: ",topic)
+        print("Payload: ",msg)
         response_topic="_response/"+topic
-        print("Subscribing to topic",response_topic)
         self.client.subscribe(response_topic)
-        print("Publishing message to topic",topic)
         properties=Properties(PacketTypes.PUBLISH)
         properties.ResponseTopic=response_topic
         self.client.on_message = self.on_message
         self.client.subscribe(response_topic)
         self.client.publish(topic,msg,properties=properties)
+        self.lock.acquire()
+        return self.response
+
+def print_json(msg):
+    json_formatted_str = json.dumps(msg, indent=2)
+    print("Response: ",json_formatted_str)
 
 if __name__ == "__main__":
     dab_test_client = Dab_Client()
     dab_test_client.connect("localhost",1883)
-    dab_test_client.send_cmd("dab/device/info","{}")
-    sleep(1)
+    response = dab_test_client.request("dab/device/info")
+    print_json(response)
+    response = dab_test_client.request("dab/operations/list")
+    print_json(response)
     dab_test_client.disconnect()
