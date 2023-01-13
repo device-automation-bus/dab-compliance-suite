febed0041ac42f63f0760e093446986c1cd8f86e
Denis Yuji Shimizu <denis.shimizu@collabora.com>
2023-01-13 14:11:47 -0300

    separate in classes

--- main.py
+++ main.py
@@ -1,44 +1,44 @@
 from paho.mqtt.properties import Properties
 from paho.mqtt.packettypes import PacketTypes 
 import paho.mqtt.client as mqtt #import the client1
+import json
+import threading
 
-broker_address="localhost"
-broker_port=1883
+class Dab_Client:
+    # def __init__(self,broker_address,broker_port):
+    
+    def on_message(self, client, userdata, message):
+        print("Received: ")    
+        json_object = json.loads(message.payload)
+        json_formatted_str = json.dumps(json_object, indent=2)
+        print(json_formatted_str)
+        print("on topic: ",message.topic)
+        self.client.disconnect()
 
-def on_message(client, userdata, message):
-    
-    import json
-    print("Received: ")    
-    json_object = json.loads(message.payload)
-    json_formatted_str = json.dumps(json_object, indent=2)
-    print(json_formatted_str)
-    print("on topic: ",message.topic)
-    
-    client.disconnect()
+    def connect(self,broker_address,broker_port):
+        print("Creating new Client instance")
+        self.client = mqtt.Client("mqtt5_client",protocol=mqtt.MQTTv5) 
+        
+        print("Connecting to MQTT broker")
+        self.client.connect(broker_address, port=broker_port)
+        
+        topic="dab/device/info5"
+        response_topic="_response/"+topic
+        print("Subscribing to topic",response_topic)
+        self.client.subscribe(response_topic)
+        print("Publishing message to topic",topic)
+        properties=Properties(PacketTypes.PUBLISH)
+        properties.ResponseTopic=response_topic
+        self.client.on_message = self.on_message
+        self.client.subscribe(response_topic)
+        self.client.publish(topic,"{}",properties=properties)
 
-def main():
-    print("Creating new Client instance")
-    client = mqtt.Client("mqtt5_client",protocol=mqtt.MQTTv5) 
-    
-    print("Connecting to MQTT broker")
-    client.connect(broker_address, port=broker_port)
-    
-    topic="dab/applications/list"
-    response_topic="_response/"+topic
-    print("Subscribing to topic",response_topic)
-    client.subscribe(response_topic)
-    print("Publishing message to topic",topic)
-    properties=Properties(PacketTypes.PUBLISH)
-    properties.ResponseTopic=response_topic
-    # Set the callback function
-    client.on_message = on_message
-    # Subscribe to a topic
-    client.subscribe(response_topic)
-    # Publish to a topic
-    client.publish("dab/applications/list","{}",properties=properties)
-    # Start the network loop to receive messages
-    client.loop_forever()
+    def loop(self):
+        self.client.loop_forever()
 
 
 if __name__ == "__main__":
-    main()
+    dab_test_client = Dab_Client()
+    dab_test_client.connect("localhost",1883)
+    dab_test_client.loop()
+    
