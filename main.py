7f1a5126c619209a677c2a2d815504d68cac5c80
Denis Yuji Shimizu <denis.shimizu@collabora.com>
2023-01-13 14:28:12 -0300

    add multi thread, add example

--- main.py
+++ main.py
@@ -1,28 +1,29 @@
+from time import sleep
+import json
 from paho.mqtt.properties import Properties
 from paho.mqtt.packettypes import PacketTypes 
-import paho.mqtt.client as mqtt #import the client1
-import json
-import threading
+import paho.mqtt.client as mqtt
 
 class Dab_Client:
-    # def __init__(self,broker_address,broker_port):
-    
     def on_message(self, client, userdata, message):
         print("Received: ")    
         json_object = json.loads(message.payload)
         json_formatted_str = json.dumps(json_object, indent=2)
         print(json_formatted_str)
         print("on topic: ",message.topic)
+        self.disconnect()
+
+    def disconnect(self):
         self.client.disconnect()
 
     def connect(self,broker_address,broker_port):
         print("Creating new Client instance")
         self.client = mqtt.Client("mqtt5_client",protocol=mqtt.MQTTv5) 
-        
         print("Connecting to MQTT broker")
         self.client.connect(broker_address, port=broker_port)
-        
-        topic="dab/device/info5"
+        self.client.loop_start()
+    
+    def send_cmd(self,topic,msg):
         response_topic="_response/"+topic
         print("Subscribing to topic",response_topic)
         self.client.subscribe(response_topic)
@@ -31,14 +32,11 @@ class Dab_Client:
         properties.ResponseTopic=response_topic
         self.client.on_message = self.on_message
         self.client.subscribe(response_topic)
-        self.client.publish(topic,"{}",properties=properties)
-
-    def loop(self):
-        self.client.loop_forever()
-
+        self.client.publish(topic,msg,properties=properties)
 
 if __name__ == "__main__":
     dab_test_client = Dab_Client()
     dab_test_client.connect("localhost",1883)
-    dab_test_client.loop()
-    
+    dab_test_client.send_cmd("dab/device/info","{}")
+    sleep(1)
+    dab_test_client.disconnect()
---
+++ simple_command.py
@@ -0,0 +1,44 @@
+from paho.mqtt.properties import Properties
+from paho.mqtt.packettypes import PacketTypes 
+import paho.mqtt.client as mqtt #import the client1
+
+broker_address="localhost"
+broker_port=1883
+
+def on_message(client, userdata, message):
+    
+    import json
+    print("Received: ")    
+    json_object = json.loads(message.payload)
+    json_formatted_str = json.dumps(json_object, indent=2)
+    print(json_formatted_str)
+    print("on topic: ",message.topic)
+    
+    client.disconnect()
+
+def main():
+    print("Creating new Client instance")
+    client = mqtt.Client("mqtt5_client",protocol=mqtt.MQTTv5) 
+    
+    print("Connecting to MQTT broker")
+    client.connect(broker_address, port=broker_port)
+    
+    topic="dab/device/info5"
+    response_topic="_response/"+topic
+    print("Subscribing to topic",response_topic)
+    client.subscribe(response_topic)
+    print("Publishing message to topic",topic)
+    properties=Properties(PacketTypes.PUBLISH)
+    properties.ResponseTopic=response_topic
+    # Set the callback function
+    client.on_message = on_message
+    # Subscribe to a topic
+    client.subscribe(response_topic)
+    # Publish to a topic
+    client.publish(topic,"{}",properties=properties)
+    # Start the network loop to receive messages
+    client.loop_forever()
+
+
+if __name__ == "__main__":
+    main()
