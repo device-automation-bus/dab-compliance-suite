79c4f0f58fd4f200061b03edf2b3980726f36e68
Denis Yuji Shimizu <denis.shimizu@collabora.com>
2023-01-13 15:46:27 -0300

    test if all operations exist

--- main.py
+++ main.py
@@ -4,23 +4,57 @@ class DabTester:
     def __init__(self):
         self.dab_test_client = DabClient()
         self.dab_test_client.connect("localhost",1883)
+        self.operations = [
+            "dab/operations/list",
+            "dab/applications/list",
+            "dab/applications/launch",
+            "dab/applications/launch-with-content",
+            "dab/applications/get-state",
+            "dab/applications/exit",
+            "dab/device/info",
+            "dab/system/restart",
+            "dab/system/settings/list",
+            "dab/system/settings/get",
+            "dab/system/settings/set",
+            "dab/input/key/list",
+            "dab/input/key-press",
+            "dab/input/long-key-press",
+            "dab/output/image",
+            "dab/device-telemetry/start",
+            "dab/device-telemetry/stop",
+            "dab/app-telemetry/start",
+            "dab/app-telemetry/stop",
+            "dab/health-check/get",
+            "dab/voice/list",
+            "dab/voice/set",
+            "dab/voice/send-audio",
+            "dab/voice/send-text",
+            "dab/version",
+            "dab/system/language/list",
+            "dab/system/language/get",
+            "dab/system/language/set",
+        ]
 
     def execute_cmd(self,operation,request="{}"):
         self.dab_test_client.request(operation,request)
         if self.dab_test_client.response['status'] == 200:
-            print("OK")
             return 0
         else:
-            print("ERROR")
             return 1
-            
+    
+    def Run(self):
+        for operation in self.operations:
+            if self.execute_cmd(operation) == 0:
+                print("[ OK ] ",end='')
+            else:
+                print("[ Error:",self.dab_test_client.response['status'],'] ',end='')
+            print("operation:",operation)
+
     def Close(self):
         self.dab_test_client.disconnect()
 
 if __name__ == "__main__":
-
     Tester = DabTester()
-    Tester.execute_cmd("dab/device/info")
-    Tester.execute_cmd("dab/operations/list")
+    Tester.Run()
     Tester.Close()
 
