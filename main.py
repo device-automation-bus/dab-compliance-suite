ece8659bc9c013d903b6e1e43eef873794427f4f
Denis Yuji Shimizu <denis.shimizu@collabora.com>
2023-01-16 03:19:51 -0300

    move test case lists to increase visibility

--- main.py
+++ main.py
@@ -4,6 +4,38 @@ import dab.applications
 import dab.system
 import argparse
 
+# Implement the test cases
+Test_Cases = [
+    ("dab/operations/list",'{}',Default_Test),
+    ("dab/applications/list",'{}',Default_Test),
+    ("dab/applications/launch",'{"appId": "Cobalt"}',dab.applications.launch),
+    ("dab/applications/launch-with-content",'{}',Default_Test),
+    ("dab/applications/get-state",'{"appId": "Cobalt"}',Default_Test),
+    ("dab/applications/exit",'{"appId": "Cobalt"}',dab.applications.exit),
+    ("dab/device/info",'{}',Default_Test),
+    ("dab/system/settings/list",'{}',Default_Test),
+    ("dab/system/settings/get",'{}',Default_Test),
+    ("dab/system/settings/set",'{}',Default_Test),
+    ("dab/input/key/list",'{}',Default_Test),
+    ("dab/input/key-press",'{"keyCode": "KEY_DOWN"}',Default_Test),
+    ("dab/input/long-key-press",'{}',Default_Test),
+    ("dab/output/image",'{}',Default_Test),
+    ("dab/device-telemetry/start",'{}',Default_Test),
+    ("dab/device-telemetry/stop",'{}',Default_Test),
+    ("dab/app-telemetry/start",'{}',Default_Test),
+    ("dab/app-telemetry/stop",'{}',Default_Test),
+    ("dab/health-check/get",'{}',Default_Test),
+    ("dab/voice/list",'{}',Default_Test),
+    ("dab/voice/set",'{}',Default_Test),
+    ("dab/voice/send-audio",'{}',Default_Test),
+    ("dab/voice/send-text",'{}',Default_Test),
+    ("dab/version",'{}',Default_Test),
+    ("dab/system/language/list",'{}',Default_Test),
+    ("dab/system/language/get",'{}',Default_Test),
+    ("dab/system/language/set",'{}',Default_Test),
+    ("dab/system/restart",'{}',dab.system.restart),
+]
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-v","--verbose", 
@@ -18,37 +50,6 @@ if __name__ == "__main__":
     
     # Use the DabTester
     Tester = DabTester()
-    # Implement the test cases
-    Test_Cases = [
-        ("dab/operations/list",'{}',Default_Test),
-        ("dab/applications/list",'{}',Default_Test),
-        ("dab/applications/launch",'{"appId": "Cobalt"}',dab.applications.launch),
-        ("dab/applications/launch-with-content",'{}',Default_Test),
-        ("dab/applications/get-state",'{"appId": "Cobalt"}',Default_Test),
-        ("dab/applications/exit",'{"appId": "Cobalt"}',dab.applications.exit),
-        ("dab/device/info",'{}',Default_Test),
-        ("dab/system/settings/list",'{}',Default_Test),
-        ("dab/system/settings/get",'{}',Default_Test),
-        ("dab/system/settings/set",'{}',Default_Test),
-        ("dab/input/key/list",'{}',Default_Test),
-        ("dab/input/key-press",'{"keyCode": "KEY_DOWN"}',Default_Test),
-        ("dab/input/long-key-press",'{}',Default_Test),
-        ("dab/output/image",'{}',Default_Test),
-        ("dab/device-telemetry/start",'{}',Default_Test),
-        ("dab/device-telemetry/stop",'{}',Default_Test),
-        ("dab/app-telemetry/start",'{}',Default_Test),
-        ("dab/app-telemetry/stop",'{}',Default_Test),
-        ("dab/health-check/get",'{}',Default_Test),
-        ("dab/voice/list",'{}',Default_Test),
-        ("dab/voice/set",'{}',Default_Test),
-        ("dab/voice/send-audio",'{}',Default_Test),
-        ("dab/voice/send-text",'{}',Default_Test),
-        ("dab/version",'{}',Default_Test),
-        ("dab/system/language/list",'{}',Default_Test),
-        ("dab/system/language/get",'{}',Default_Test),
-        ("dab/system/language/set",'{}',Default_Test),
-        ("dab/system/restart",'{}',dab.system.restart),
-    ]
     
     Tester.verbose = args.verbose
     
