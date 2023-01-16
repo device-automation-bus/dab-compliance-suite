366ec1b0b88997c1a1a738267fa8fcd85b92baaf
Denis Yuji Shimizu <denis.shimizu@collabora.com>
2023-01-15 23:47:43 -0300

    improve struct of directory

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 from DabTester import DabTester
 from DabTester import Default_Test
-import applications 
-import system
+import dab.applications
+import dab.system
 
 if __name__ == "__main__":
     Tester = DabTester()
     Test_Cases = [
         ("dab/operations/list",'{}',Default_Test),
         ("dab/applications/list",'{}',Default_Test),
-        ("dab/applications/launch",'{"appId": "Cobalt"}',applications.launch),
+        ("dab/applications/launch",'{"appId": "Cobalt"}',dab.applications.launch),
         ("dab/applications/launch-with-content",'{}',Default_Test),
         ("dab/applications/get-state",'{"appId": "Cobalt"}',Default_Test),
-        ("dab/applications/exit",'{"appId": "Cobalt"}',applications.exit),
+        ("dab/applications/exit",'{"appId": "Cobalt"}',dab.applications.exit),
         ("dab/device/info",'{}',Default_Test),
         ("dab/system/settings/list",'{}',Default_Test),
         ("dab/system/settings/get",'{}',Default_Test),
@@ -33,7 +33,7 @@ if __name__ == "__main__":
         ("dab/system/language/list",'{}',Default_Test),
         ("dab/system/language/get",'{}',Default_Test),
         ("dab/system/language/set",'{}',Default_Test),
-        ("dab/system/restart",'{}',system.restart),
+        ("dab/system/restart",'{}',dab.system.restart),
     ]
     
     Tester.verbose = False
