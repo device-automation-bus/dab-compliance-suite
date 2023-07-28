2362af1ca7942c55dc9f3ce9434ffcfb38982c1c
Denis Yuji Shimizu <denis.shimizu@collabora.com>
2023-07-28 02:04:30 -0300

    fix duplicated string on a voice entry

--- voice_text.py
+++ voice_text.py
@@ -5,7 +5,7 @@ import dab.voice
 # Voice action steps
 SEND_VOICE_TEXT_TEST_CASES = [
     ("voice/send-text",'{"requestText" : "Play lady Gaga music on YouTube", "voiceSystem": "Alexa"}', dab.voice.send_text, "Are you on search page with Lady Gaga?", "Voice launch Lady gaga"),
-    ("voice/send-text",'{"requestText" : "Press enter", "voiceSystem": "Alexa"}', dab.voice.send_text, "Is video playing?", "Voice play video", "Voice play video"),
+    ("voice/send-text",'{"requestText" : "Press enter", "voiceSystem": "Alexa"}', dab.voice.send_text, "Is video playing?", "Voice play video"),
     ("voice/send-text",'{"requestText" : "Play video", "voiceSystem": "Alexa"}', dab.voice.send_text, "If video was not playing, is it playing now?", "Voice resume video"),
     ("voice/send-text",'{"requestText" : "Set volume 0", "voiceSystem": "Alexa"}', dab.voice.send_text, "Did volume of the video changed?", "voice mute"),
     ("voice/send-text",'{"requestText" : "Set volume 5", "voiceSystem": "Alexa"}', dab.voice.send_text, "Did volume of the video changed?", "voice volume up"),
