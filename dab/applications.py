67a04f1e52b89e646fc1257182f74013bc5a0aeb
Wei Wang <david36ww@hotmail.com>
2023-11-08 18:49:12 +0000

    Update app name

--- dab/applications.py
+++ dab/applications.py
@@ -9,7 +9,7 @@ def launch(test_result, durationInMs=0,expectedLatencyMs=0):
     if response['status'] != 200:
         return False
     sleep(5)
-    return YesNoQuestion(test_result, "Cobalt started?") and Default_Validations(test_result, durationInMs, expectedLatencyMs)
+    return YesNoQuestion(test_result, "App started?") and Default_Validations(test_result, durationInMs, expectedLatencyMs)
 
 def launch_with_content(test_result, durationInMs=0,expectedLatencyMs=0):
     dab_response_validator.validate_dab_response_schema(test_result.response)
@@ -17,7 +17,7 @@ def launch_with_content(test_result, durationInMs=0,expectedLatencyMs=0):
     if response['status'] != 200:
         return False
     sleep(5)
-    return YesNoQuestion(test_result, "Cobalt started with playback?") and Default_Validations(test_result, durationInMs, expectedLatencyMs)
+    return YesNoQuestion(test_result, "App started with playback?") and Default_Validations(test_result, durationInMs, expectedLatencyMs)
     
 def exit(test_result, durationInMs=0,expectedLatencyMs=0):
     dab_response_validator.validate_exit_application_response_schema(test_result.response)
@@ -25,7 +25,7 @@ def exit(test_result, durationInMs=0,expectedLatencyMs=0):
     if response['status'] != 200:
         return False
     sleep(5)
-    return YesNoQuestion(test_result, "Cobalt exited?") and Default_Validations(test_result, durationInMs, expectedLatencyMs)
+    return YesNoQuestion(test_result, "App exited?") and Default_Validations(test_result, durationInMs, expectedLatencyMs)
 
 def list(test_result, durationInMs=0,expectedLatencyMs=0):
     dab_response_validator.validate_list_applications_response_schema(test_result.response)
