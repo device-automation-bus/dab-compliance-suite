3dcd2bea95e57edcf15351e8b9c2985a56bd1700
Wei Wang <david36ww@hotmail.com>
2024-02-07 16:19:22 -0800

    Update dab/operations.py
    Co-authored-by: Jakub Adam <jakub.adam@collabora.com>

--- dab/operations.py
+++ dab/operations.py
@@ -13,6 +13,7 @@ def list(test_result, durationInMs=0,expectedLatencyMs=0):
     response  = jsons.loads(test_result.response)
     if response['status'] != 200:
         return False
-    EnforcementManager().add_supported_operation(response.operations)
+    for operation in response['operations']:
+        EnforcementManager().add_supported_operation(operation)
     sleep(0.1)
     return Default_Validations(test_result, durationInMs, expectedLatencyMs)
