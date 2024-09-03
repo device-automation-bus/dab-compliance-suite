740f6531502788baca480c6448785095a0a392ec
Chen Mingsu <chenms.chen@zeasn.com>
2024-09-03 14:54:02 +0800

    test suite always using 'AmazonAlexa' for voice related test, change to read config.va

--- voice_text.py
+++ voice_text.py
@@ -1,3 +1,4 @@
+import config
 import dab.applications
 import dab.system
 import dab.voice
@@ -6,13 +7,13 @@ from util.enforcement_manager import EnforcementManager
 # Voice action steps
 SEND_VOICE_TEXT_TEST_CASES = [
     ("voice/list",'{}', dab.voice.list, 200, "Voice List"),
-    ("voice/send-text",f'{{"requestText" : "Play lady Gaga music on YouTube", "voiceSystem": "{EnforcementManager().get_voice_assistant()}"}}', dab.voice.send_text, "Are you on search page with Lady Gaga?", "Voice launch Lady gaga"),
-    ("voice/send-text",f'{{"requestText" : "Press enter", "voiceSystem": "{EnforcementManager().get_voice_assistant()}"}}', dab.voice.send_text, "Is video playing?", "Voice play video"),
-    ("voice/send-text",f'{{"requestText" : "Play video", "voiceSystem": "{EnforcementManager().get_voice_assistant()}"}}', dab.voice.send_text, "If video was not playing, is it playing now?", "Voice resume video"),
-    ("voice/send-text",f'{{"requestText" : "Set volume 0", "voiceSystem": "{EnforcementManager().get_voice_assistant()}"}}', dab.voice.send_text, "Did volume of the video changed?", "voice mute"),
-    ("voice/send-text",f'{{"requestText" : "Set volume 5", "voiceSystem": "{EnforcementManager().get_voice_assistant()}"}}', dab.voice.send_text, "Did volume of the video changed?", "voice volume up"),
-    ("voice/send-text",f'{{"requestText" : "Pause Video", "voiceSystem": "{EnforcementManager().get_voice_assistant()}"}}', dab.voice.send_text, "Did video paused?", "voice pause"),
-    ("voice/send-text",f'{{"requestText" : "Fast forward video", "voiceSystem": "{EnforcementManager().get_voice_assistant()}"}}', dab.voice.send_text, "Did video playback fast forward?", "voice fastforward"),
-    ("voice/send-text",f'{{"requestText" : "Rewind video", "voiceSystem": "{EnforcementManager().get_voice_assistant()}"}}', dab.voice.send_text, "Did video playback rewind?", "voice rewind"),
-    ("voice/send-text",f'{{"requestText" : "Exit to main menu", "voiceSystem": "{EnforcementManager().get_voice_assistant()}"}}', dab.voice.send_text, "Are you on main menu?", "voice exit"),
+    ("voice/send-text",f'{{"requestText" : "Play lady Gaga music on YouTube", "voiceSystem": "{config.va}"}}', dab.voice.send_text, "Are you on search page with Lady Gaga?", "Voice launch Lady gaga"),
+    ("voice/send-text",f'{{"requestText" : "Press enter", "voiceSystem": "{config.va}"}}', dab.voice.send_text, "Is video playing?", "Voice play video"),
+    ("voice/send-text",f'{{"requestText" : "Play video", "voiceSystem": "{config.va}"}}', dab.voice.send_text, "If video was not playing, is it playing now?", "Voice resume video"),
+    ("voice/send-text",f'{{"requestText" : "Set volume 0", "voiceSystem": "{config.va}"}}', dab.voice.send_text, "Did volume of the video changed?", "voice mute"),
+    ("voice/send-text",f'{{"requestText" : "Set volume 5", "voiceSystem": "{config.va}"}}', dab.voice.send_text, "Did volume of the video changed?", "voice volume up"),
+    ("voice/send-text",f'{{"requestText" : "Pause Video", "voiceSystem": "{config.va}"}}', dab.voice.send_text, "Did video paused?", "voice pause"),
+    ("voice/send-text",f'{{"requestText" : "Fast forward video", "voiceSystem": "{config.va}"}}', dab.voice.send_text, "Did video playback fast forward?", "voice fastforward"),
+    ("voice/send-text",f'{{"requestText" : "Rewind video", "voiceSystem": "{config.va}"}}', dab.voice.send_text, "Did video playback rewind?", "voice rewind"),
+    ("voice/send-text",f'{{"requestText" : "Exit to main menu", "voiceSystem": "{config.va}"}}', dab.voice.send_text, "Are you on main menu?", "voice exit"),
 ]
