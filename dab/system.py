f996b2bbcea03ffc11ee0aa47163d8b590864185
hariprasadh <hariprasadh@google.com>
2026-09-08 05:49:14 +0000

    Remove setup/skip test from conformance tool

--- dab/system.py
+++ dab/system.py
@@ -114,17 +114,6 @@ def stop_log_collection(test_result, durationInMs=0, expectedLatencyMs=0):
     sleep(0.1)
     return Default_Validations(test_result, durationInMs, expectedLatencyMs)
 
-def setup_skip(test_result, durationInMs=0, expectedLatencyMs=0):
-    try:
-        dab_response_validator.validate_dab_response_schema(test_result.response)
-    except Exception as error:
-        print("Schema error:", error)
-        return False
-    response = jsons.loads(test_result.response)
-    if response['status'] != 200:
-        return False
-    sleep(0.1)
-    return Default_Validations(test_result, durationInMs, expectedLatencyMs)
 
 def power_mode_get(test_result, durationInMs=0, expectedLatencyMs=0):
     try:
